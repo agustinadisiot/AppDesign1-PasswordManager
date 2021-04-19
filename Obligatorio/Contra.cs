a2a9e53e249ccab7c92ca051185145e36f0c4ae1
Agustina <agustinadisiotvevo@gmail.com>
2021-04-19 16:02:03 -0300

    Emprolijado de nuevo el codigo de getNivelSeguridad.

--- Obligatorio/Contra.cs
+++ Obligatorio/Contra.cs
@@ -42,17 +42,14 @@ namespace Obligatorio
             bool tieneMin = false, tieneMay = false, tieneNum = false, tieneSim = false;
             foreach (int c in this.clave)
             {
-                if(!tieneMay || !tieneMin || !tieneMin || !tieneSim)
+                if (!tieneMay || !tieneMin || !tieneMin || !tieneSim)
                 {
                     tieneMin = tieneMin || (c >= 97 && c <= 122);
                     tieneMay = tieneMay || (c >= 65 && c <= 90);
                     tieneNum = tieneNum || (c >= 48 && c <= 57);
                     tieneSim = tieneSim || ((c >= 32 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || (c >= 123 && c <= 126));
                 }
-                else
-                {
-                    return verdeOscuro;
-                }
+                else return verdeOscuro;
             }
             if(tieneMin && tieneMay) return (tieneNum && tieneSim) ? verdeOscuro : verdeClaro;
 
