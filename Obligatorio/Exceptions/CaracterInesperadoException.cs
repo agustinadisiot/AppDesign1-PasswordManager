37c6c44cc75624fdb72536f5a0e9c428a993a951
Santiago Diaz <SD240926@fi365.ort.edu.uy>
2021-04-19 17:20:46 -0300

    agregada nueva excepcion CaracterInesperadoException

---
+++ Obligatorio/Exceptions/CaracterInesperadoException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+namespace Obligatorio
+
+{
+    [Serializable]
+    public class CaracterInesperadoException : Exception
+    {
+        private string message;
+
+        public override string Message
+        {
+            get { return message; }
+        }
+
+        public CaracterInesperadoException()
+        {
+            this.message = "La variable incluye uno o mas caracteres incorrectos.";
+        }
+    }
+}
