f99cf90c508f7d3d5341bf3f26fdf906b9b601bf
Santiago Diaz <SD240926@fi365.ort.edu.uy>
2021-04-25 16:25:15 -0300

    Agregada prueba en usuario que controla si ya existe una contraseña con diferente clave e igual usuario y sitio en un usuario con una categoria.

--- TestsObligatorio/TestUsuario.cs
+++ TestsObligatorio/TestUsuario.cs
@@ -460,6 +460,36 @@ namespace TestsObligatorio
             Assert.AreEqual(false, usuario.yaExisteContra(contraIgual));
         }
 
+        //Prueba que agrega una Contra y despues pregunta si ya existe una con mismo sitio y usuario, devuelve true a pesar de tener diferentes claves.
+        [TestMethod]
+        public void testUsuarioYaExisteContraDiferentesClaves()
+        {
+            Usuario usuario = new Usuario()
+            {
+                Nombre = "Usuario",
+                ContraMaestra = "contra123"
+            };
+            Categoria categoria = new Categoria()
+            {
+                Nombre = "Trabajo"
+            };
+            Contra contra = new Contra()
+            {
+                Sitio = "www.ort.edu.uy",
+                UsuarioContra = "111111",
+                Clave = "12345678"
+            };
+            categoria.agregarContra(contra);
+            usuario.agregarCategoria(categoria);
+            Contra contraIgual = new Contra()
+            {
+                Sitio = "www.ort.edu.uy",
+                UsuarioContra = "111111",
+                Clave = "12345678"
+            };
+            Assert.AreEqual(true, usuario.yaExisteContra(contraIgual));
+        }
+
     }
 }
 
