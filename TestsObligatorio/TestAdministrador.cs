57188581affcc2233a6f3a1c11e497a82be43294
Santiago Diaz <SD240926@fi365.ort.edu.uy>
2021-04-29 16:17:09 -0300

    Corregido errores de tipografia, variables con nombres muy cortos y eliminado el 'test' al principio de todas las pruebas, en TestAdministrador.

--- TestsObligatorio/TestAdministrador.cs
+++ TestsObligatorio/TestAdministrador.cs
@@ -13,9 +13,9 @@ namespace TestsObligatorio
     public class TestAdministrador
     {
 
-        //Prueba si al comenzar el administrador esta vacío.
+        //Prueba si al comenzar el administrador la lista de usuarios esta vacía.
         [TestMethod]
-        public void testEsListaUsuariosVaciaAlPrincipio()
+        public void EsListaUsuariosVaciaAlPrincipio()
         {
             AdminContras administrador = new AdminContras();
             Assert.AreEqual(true, administrador.esListaUsuariosVacia());
@@ -23,112 +23,112 @@ namespace TestsObligatorio
 
         //Prueba si al agregar un usuario, esListaUsuariosVacia da false
         [TestMethod]
-        public void testAdministradorConUsuarios()
+        public void AdministradorConUsuarios()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario
+            Usuario usuario = new Usuario
             {
                 Nombre = "Roberto"
             };
-            administrador.agregarUsuario(u1);
+            administrador.agregarUsuario(usuario);
             Assert.AreEqual(false, administrador.esListaUsuariosVacia());
         }
 
         //Prueba si al agregar un usuario sin nombre, de una exception.
         [TestMethod]
-        public void testAdministradorAgregarUsuarioSinNombre()
+        public void AdministradorAgregarUsuarioSinNombre()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario();
-            Assert.ThrowsException<ObjetoIncompletoException>(() => administrador.agregarUsuario(u1));
+            Usuario usuario = new Usuario();
+            Assert.ThrowsException<ObjetoIncompletoException>(() => administrador.agregarUsuario(usuario));
         }
 
 
         //Prueba si al agregar dos usuarios, esListaUsuariosVacia sigue dando false
         [TestMethod]
-        public void testAdministradorEsListaUsuariosVaciaConDosUsuarios()
+        public void AdministradorEsListaUsuariosVaciaConDosUsuarios()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario
+            Usuario usuario = new Usuario
             {
                 Nombre = "Roberto"
             };
-            administrador.agregarUsuario(u1);
+            administrador.agregarUsuario(usuario);
             Assert.AreEqual(false, administrador.esListaUsuariosVacia());
-            Usuario u2 = new Usuario
+            Usuario usuario2 = new Usuario
             {
                 Nombre = "Pedro"
             };
-            administrador.agregarUsuario(u2);
+            administrador.agregarUsuario(usuario2);
             Assert.AreEqual(false, administrador.esListaUsuariosVacia());
         }
 
         //Prueba si al pedir el usuario, devuelve el mismo.
         [TestMethod]
-        public void testAdministradorPedirUsuario()
+        public void AdministradorPedirUsuario()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario
+            Usuario usuario = new Usuario
             {
                 Nombre = "Roberto"
             };
-            administrador.agregarUsuario(u1);
-            Assert.AreEqual(u1, administrador.getUsuario("Roberto"));
+            administrador.agregarUsuario(usuario);
+            Assert.AreEqual(usuario, administrador.getUsuario("Roberto"));
         }
 
-        //Prueba si al agregar dos usuarios, y pedir el primer usuario agregado por nombre, devuelve el correcto.
+        //Prueba si al agregar dos usuarios, y pedir el primer usuario agregado, por nombre, devuelve el correcto.
         [TestMethod]
-        public void testAdministradorPedirUsuarioPrimeroConDosAgregados()
+        public void AdministradorPedirUsuarioPrimeroConDosAgregados()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario
+            Usuario usuario = new Usuario
             {
                 Nombre = "Roberto"
             };
-            administrador.agregarUsuario(u1);
-            Usuario u2 = new Usuario
+            administrador.agregarUsuario(usuario);
+            Usuario usuario2 = new Usuario
             {
                 Nombre = "Pedro"
             };
-            administrador.agregarUsuario(u2);
-            Assert.AreEqual(u1, administrador.getUsuario("Roberto"));
+            administrador.agregarUsuario(usuario2);
+            Assert.AreEqual(usuario, administrador.getUsuario("Roberto"));
         }
 
 
         //Prueba si al agregar dos usuarios, y pedir el segundo usuario agregado por nombre, devuelve el correcto.
         [TestMethod]
-        public void testAdministradorPedirUsuarioSegundoConDosAgregados()
+        public void AdministradorPedirUsuarioSegundoConDosAgregados()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario
+            Usuario usuario = new Usuario
             {
                 Nombre = "Roberto"
             };
-            administrador.agregarUsuario(u1);
-            Usuario u2 = new Usuario
+            administrador.agregarUsuario(usuario);
+            Usuario usuario2 = new Usuario
             {
                 Nombre = "Pedro"
             };
-            administrador.agregarUsuario(u2);
-            Assert.AreEqual(u2, administrador.getUsuario("Pedro"));
+            administrador.agregarUsuario(usuario2);
+            Assert.AreEqual(usuario2, administrador.getUsuario("Pedro"));
         }
 
 
         //Prueba si al agregar dos usuarios, y pedir un usuario con un nombre no agregado, devuelve un error.
         [TestMethod]
-        public void testAdministradorPedirUsuarioInexistente()
+        public void AdministradorPedirUsuarioInexistente()
         {
             AdminContras administrador = new AdminContras();
-            Usuario u1 = new Usuario
+            Usuario usuario = new Usuario
             {
                 Nombre = "Roberto"
             };
-            administrador.agregarUsuario(u1);
-            Usuario u2 = new Usuario
+            administrador.agregarUsuario(usuario);
+            Usuario usuario2 = new Usuario
             {
                 Nombre = "Pedro"
             };
-            administrador.agregarUsuario(u2);
+            administrador.agregarUsuario(usuario2);
             Assert.ThrowsException<ObjetoInexistenteException>(() => administrador.getUsuario("Hernesto"));
         }
 
