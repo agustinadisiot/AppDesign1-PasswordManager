67040ab71d0e05a5d91bd0150de4a27cd818f555
Agustina <agustinadisiotvevo@gmail.com>
2021-05-04 18:26:02 -0300

    Agregado diseño para lista de claves de un usuario.

---
+++ Interfaz/ListaClaves.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Interfaz
+{
+    partial class ListaClaves
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablaClaves = new System.Windows.Forms.DataGridView();
+            this.Categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sitio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UltimaModificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaClaves)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablaClaves
+            // 
+            this.tablaClaves.AllowUserToAddRows = false;
+            this.tablaClaves.AllowUserToDeleteRows = false;
+            this.tablaClaves.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaClaves.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Categoria,
+            this.Sitio,
+            this.Usuario,
+            this.UltimaModificacion});
+            this.tablaClaves.Location = new System.Drawing.Point(14, 21);
+            this.tablaClaves.Name = "tablaClaves";
+            this.tablaClaves.ReadOnly = true;
+            this.tablaClaves.RowHeadersVisible = false;
+            this.tablaClaves.RowHeadersWidth = 51;
+            this.tablaClaves.RowTemplate.Height = 24;
+            this.tablaClaves.Size = new System.Drawing.Size(1393, 817);
+            this.tablaClaves.TabIndex = 0;
+            // 
+            // Categoria
+            // 
+            this.Categoria.HeaderText = "Categoria";
+            this.Categoria.MinimumWidth = 6;
+            this.Categoria.Name = "Categoria";
+            this.Categoria.ReadOnly = true;
+            this.Categoria.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Categoria.Width = 125;
+            // 
+            // Sitio
+            // 
+            this.Sitio.HeaderText = "Sitio";
+            this.Sitio.MinimumWidth = 6;
+            this.Sitio.Name = "Sitio";
+            this.Sitio.ReadOnly = true;
+            this.Sitio.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Sitio.Width = 125;
+            // 
+            // Usuario
+            // 
+            this.Usuario.HeaderText = "Usuario";
+            this.Usuario.MinimumWidth = 6;
+            this.Usuario.Name = "Usuario";
+            this.Usuario.ReadOnly = true;
+            this.Usuario.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Usuario.Width = 125;
+            // 
+            // UltimaModificacion
+            // 
+            this.UltimaModificacion.HeaderText = "Ultima Modificacion";
+            this.UltimaModificacion.MinimumWidth = 6;
+            this.UltimaModificacion.Name = "UltimaModificacion";
+            this.UltimaModificacion.ReadOnly = true;
+            this.UltimaModificacion.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.UltimaModificacion.Width = 125;
+            // 
+            // ListaClaves
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tablaClaves);
+            this.Name = "ListaClaves";
+            this.Size = new System.Drawing.Size(1433, 838);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaClaves)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tablaClaves;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Categoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sitio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UltimaModificacion;
+    }
+}
---
+++ Interfaz/ListaClaves.cs
@@ -0,0 +1,53 @@
+﻿using Obligatorio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz
+{
+    public partial class ListaClaves : UserControl
+    {
+        private Usuario _usuarioActual;
+
+        public ListaClaves(Usuario usuarioAgregar)
+        {
+            InitializeComponent();
+            this._usuarioActual = usuarioAgregar;
+        }
+
+        private void CargarTabla()
+        {
+
+            List<Categoria> listaCategorias = this._usuarioActual.GetListaCategorias();
+
+            foreach (Categoria categoriaActual in listaCategorias)
+            {
+
+                string nombreCategoria = categoriaActual.Nombre;
+                List<Contra> listaContras = categoriaActual.GetListaContras();
+                CargarFila(nombreCategoria, listaContras);
+            }
+        }
+
+
+        private void CargarFila(string categoriaActual, List<Contra> listaContras)
+        {
+
+            foreach (Contra contraActual in listaContras)
+            {
+
+                string sitio = contraActual.Sitio;
+                string usuario = contraActual.UsuarioContra;
+                string ultimaModificacion = "";
+
+                this.tablaClaves.Rows.Add(categoriaActual, sitio, usuario, ultimaModificacion);
+            }
+        }
+    }
+}
