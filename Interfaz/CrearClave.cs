4b06b9fe8d09713a920821bd35d48eea1a7619ef
Agustina <agustinadisiotvevo@gmail.com>
2021-05-05 19:33:41 -0300

    Agregado codigo para cargar el combo en crear clave.

--- Interfaz/CrearClave.cs
+++ Interfaz/CrearClave.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Obligatorio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace Interfaz
 {
     public partial class CrearClave : UserControl
     {
-        public CrearClave()
+        private Usuario _usuarioActual;
+        private AdminContras _administrador;
+
+        public CrearClave(Usuario usuarioAgregar, AdminContras administradorAgregar)
         {
             InitializeComponent();
+            this._usuarioActual = usuarioAgregar;
+            this._administrador = administradorAgregar;
+            this.CargarComboBox();
+        }
+
+        private void CargarComboBox()
+        {
+            this.comboCategorias.Items.Clear();
+            List<Categoria> lista = this._usuarioActual.GetListaCategorias();
+
+            foreach (Categoria actual in lista)
+            {
+                string nombre = actual.Nombre;
+                this.comboCategorias.Items.Add(nombre);
+                
+            }
+
         }
     }
 }
