37f1ae70ad2751208c9c8b4e01f9d6b7fb95ebeb
Joaquin meerhoff <jm247096@fi365.ort.edu.uy>
2021-05-05 16:10:18 -0300

    Resuelto problema de perdida de interfaz

---
+++ Obligatorio/Interfaz/VentanaPrincipal.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Interfaz
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDrawer = new System.Windows.Forms.Panel();
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelDrawer
+            // 
+            this.panelDrawer.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelDrawer.Location = new System.Drawing.Point(0, 0);
+            this.panelDrawer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelDrawer.Name = "panelDrawer";
+            this.panelDrawer.Size = new System.Drawing.Size(252, 838);
+            this.panelDrawer.TabIndex = 0;
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrincipal.Location = new System.Drawing.Point(252, 0);
+            this.panelPrincipal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(1433, 838);
+            this.panelPrincipal.TabIndex = 1;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.Controls.Add(this.panelPrincipal);
+            this.Controls.Add(this.panelDrawer);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "VentanaPrincipal";
+            this.Text = "Ventana Principal";
+            this.Load += new System.EventHandler(this.VentanaPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDrawer;
+        private System.Windows.Forms.Panel panelPrincipal;
+    }
+}
+
---
+++ Obligatorio/Interfaz/VentanaPrincipal.cs
@@ -0,0 +1,93 @@
+﻿using Obligatorio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz
+{
+    public partial class VentanaPrincipal : Form
+    {
+
+        private AdminContras _administrador;
+        private Usuario _usuarioActual;
+
+        public VentanaPrincipal()
+        {
+            this._administrador = new AdminContras();
+
+            Usuario usuarioPrueba = new Usuario();
+            usuarioPrueba.Nombre = "Roberto";
+            usuarioPrueba.ContraMaestra = "12345ABCD";
+
+            Usuario usuarioPrueba2 = new Usuario();
+            usuarioPrueba2.Nombre = "Santiago";
+            usuarioPrueba2.ContraMaestra = "ClaveUsuario123";
+
+            this._administrador.AgregarUsuario(usuarioPrueba);
+            this._administrador.AgregarUsuario(usuarioPrueba2);
+
+
+            Categoria trabajo = new Categoria() 
+            {
+                   Nombre = "Trabajo"
+            };
+
+            Categoria personal = new Categoria()
+            {
+                Nombre = "Personal"
+            };
+
+            usuarioPrueba.AgregarCategoria(trabajo);
+            usuarioPrueba.AgregarCategoria(personal);
+
+            Tarjeta tarjetaPrueba1 = new Tarjeta()
+            {
+                Nombre = "Itau",
+                Tipo = "Visa",
+                Numero = "3456567890876543",
+                Codigo = "321",
+                Vencimiento = new DateTime(2025, 7, 1)
+            };
+
+            Tarjeta tarjetaPrueba2 = new Tarjeta()
+            {
+                Nombre = "Prex",
+                Tipo = "Mastercard",
+                Numero = "2222567890876543",
+                Codigo = "8904",
+                Vencimiento = new DateTime(2025, 7, 1)
+            };
+
+            usuarioPrueba.AgregarTarjeta(tarjetaPrueba1, trabajo);
+
+            usuarioPrueba.AgregarTarjeta(tarjetaPrueba2, personal);
+
+            this._usuarioActual = usuarioPrueba;
+            InitializeComponent();
+
+        }
+
+        private void VentanaPrincipal_Load(object sender, EventArgs e)
+        {
+            CrearTarjeta crearTarjetas = new CrearTarjeta(this._usuarioActual);
+
+            crearTarjetas.AbrirListaTarjetas_Event += new EventHandler(this.AbrirListaTarjetas_Handler);
+            this.panelPrincipal.Controls.Add(crearTarjetas);
+        }
+
+        protected void AbrirListaTarjetas_Handler(object sender, EventArgs e) {
+            this.panelPrincipal.Controls.Clear();
+
+            ListaTarjetas listarTarjetas = new ListaTarjetas(this._usuarioActual, this._administrador);
+
+            this.panelPrincipal.Controls.Add(listarTarjetas);
+        }
+
+    }
+}
