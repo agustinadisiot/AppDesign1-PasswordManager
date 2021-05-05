ef4409b22d13c11443cb9f11a74fa991a6945db8
Agustina <agustinadisiotvevo@gmail.com>
2021-05-05 17:46:43 -0300

    Agregada prueba de get para la lista de usuarios en administrador que compara listas de usuarios.

--- TestsObligatorio/TestAdministrador.cs
+++ TestsObligatorio/TestAdministrador.cs
@@ -193,6 +193,31 @@ namespace TestsObligatorio
 
             Assert.IsNotNull(administrador.GetListaUsuarios());
         }
+
+        [TestMethod]
+        public void AdministradorGetListaUsuariosEsIgual()
+        {
+            AdminContras administrador = new AdminContras();
+            Usuario usuario1 = new Usuario()
+            {
+                Nombre = "Usuario1"
+            };
+
+            Usuario usuario2 = new Usuario()
+            {
+                Nombre = "Usuario2"
+            };
+
+            administrador.AgregarUsuario(usuario1);
+            administrador.AgregarUsuario(usuario2);
+
+            List<Usuario> usuariosComparar = new List<Usuario>
+            {
+                usuario1,
+                usuario2
+            };
+            Assert.AreEqual(true, usuariosComparar.SequenceEqual(administrador.GetListaUsuarios())); ;
+        }
     }
 
 }
