1128b7356c54a6a6a8a75e553a8ccf74fe792c71
Agustina <agustinadisiotvevo@gmail.com>
2021-05-08 16:26:13 -0300

    Modificada interfaz del listado de claves.

--- Interfaz/InterfacesClaves/ListaClaves.Designer.cs
+++ Interfaz/InterfacesClaves/ListaClaves.Designer.cs
@@ -29,17 +29,19 @@ namespace Interfaz
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tablaClaves = new System.Windows.Forms.DataGridView();
             this.Categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Sitio = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.UltimaModificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelTitulo = new System.Windows.Forms.Label();
             this.botonAgregar = new System.Windows.Forms.Button();
+            this.botonVer = new System.Windows.Forms.Button();
+            this.botonCompartir = new System.Windows.Forms.Button();
             this.botonEliminar = new System.Windows.Forms.Button();
             this.botonModificar = new System.Windows.Forms.Button();
-            this.botonCompartir = new System.Windows.Forms.Button();
-            this.labelTitulo = new System.Windows.Forms.Label();
-            this.botonVer = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tablaClaves)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,20 +49,36 @@ namespace Interfaz
             // 
             this.tablaClaves.AllowUserToAddRows = false;
             this.tablaClaves.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tablaClaves.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.tablaClaves.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tablaClaves.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Categoria,
             this.Sitio,
             this.Usuario,
             this.UltimaModificacion});
-            this.tablaClaves.Location = new System.Drawing.Point(18, 101);
+            this.tablaClaves.Location = new System.Drawing.Point(100, 78);
             this.tablaClaves.Margin = new System.Windows.Forms.Padding(4);
             this.tablaClaves.Name = "tablaClaves";
             this.tablaClaves.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tablaClaves.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.tablaClaves.RowHeadersVisible = false;
             this.tablaClaves.RowHeadersWidth = 51;
             this.tablaClaves.RowTemplate.Height = 24;
-            this.tablaClaves.Size = new System.Drawing.Size(643, 809);
+            this.tablaClaves.Size = new System.Drawing.Size(608, 369);
             this.tablaClaves.TabIndex = 0;
             // 
             // Categoria
@@ -99,87 +117,87 @@ namespace Interfaz
             this.UltimaModificacion.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.UltimaModificacion.Width = 125;
             // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.labelTitulo.Location = new System.Drawing.Point(93, 43);
+            this.labelTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(301, 31);
+            this.labelTitulo.TabIndex = 17;
+            this.labelTitulo.Text = "Listado de Contraseñas";
+            // 
             // botonAgregar
             // 
-            this.botonAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.botonAgregar.Location = new System.Drawing.Point(942, 942);
-            this.botonAgregar.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.botonAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.botonAgregar.Location = new System.Drawing.Point(519, 467);
+            this.botonAgregar.Margin = new System.Windows.Forms.Padding(4);
             this.botonAgregar.Name = "botonAgregar";
-            this.botonAgregar.Size = new System.Drawing.Size(240, 58);
-            this.botonAgregar.TabIndex = 13;
+            this.botonAgregar.Size = new System.Drawing.Size(100, 28);
+            this.botonAgregar.TabIndex = 19;
             this.botonAgregar.Text = "Agregar";
             this.botonAgregar.UseVisualStyleBackColor = true;
             // 
+            // botonVer
+            // 
+            this.botonVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.botonVer.Location = new System.Drawing.Point(248, 467);
+            this.botonVer.Margin = new System.Windows.Forms.Padding(4);
+            this.botonVer.Name = "botonVer";
+            this.botonVer.Size = new System.Drawing.Size(100, 28);
+            this.botonVer.TabIndex = 20;
+            this.botonVer.Text = "Ver";
+            this.botonVer.UseVisualStyleBackColor = true;
+            // 
+            // botonCompartir
+            // 
+            this.botonCompartir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.botonCompartir.Location = new System.Drawing.Point(386, 467);
+            this.botonCompartir.Margin = new System.Windows.Forms.Padding(4);
+            this.botonCompartir.Name = "botonCompartir";
+            this.botonCompartir.Size = new System.Drawing.Size(100, 28);
+            this.botonCompartir.TabIndex = 21;
+            this.botonCompartir.Text = "Compartir";
+            this.botonCompartir.UseVisualStyleBackColor = true;
+            // 
             // botonEliminar
             // 
-            this.botonEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.botonEliminar.Location = new System.Drawing.Point(1230, 942);
-            this.botonEliminar.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.botonEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.botonEliminar.Location = new System.Drawing.Point(657, 467);
+            this.botonEliminar.Margin = new System.Windows.Forms.Padding(4);
             this.botonEliminar.Name = "botonEliminar";
-            this.botonEliminar.Size = new System.Drawing.Size(240, 58);
-            this.botonEliminar.TabIndex = 14;
+            this.botonEliminar.Size = new System.Drawing.Size(100, 28);
+            this.botonEliminar.TabIndex = 22;
             this.botonEliminar.Text = "Eliminar";
             this.botonEliminar.UseVisualStyleBackColor = true;
             // 
             // botonModificar
             // 
-            this.botonModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.botonModificar.Location = new System.Drawing.Point(1519, 942);
-            this.botonModificar.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.botonModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.botonModificar.Location = new System.Drawing.Point(795, 467);
+            this.botonModificar.Margin = new System.Windows.Forms.Padding(4);
             this.botonModificar.Name = "botonModificar";
-            this.botonModificar.Size = new System.Drawing.Size(240, 58);
-            this.botonModificar.TabIndex = 15;
+            this.botonModificar.Size = new System.Drawing.Size(100, 28);
+            this.botonModificar.TabIndex = 23;
             this.botonModificar.Text = "Modificar";
             this.botonModificar.UseVisualStyleBackColor = true;
-            // 
-            // botonCompartir
-            // 
-            this.botonCompartir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.botonCompartir.Location = new System.Drawing.Point(649, 942);
-            this.botonCompartir.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
-            this.botonCompartir.Name = "botonCompartir";
-            this.botonCompartir.Size = new System.Drawing.Size(240, 58);
-            this.botonCompartir.TabIndex = 16;
-            this.botonCompartir.Text = "Compartir";
-            this.botonCompartir.UseVisualStyleBackColor = true;
-            // 
-            // labelTitulo
-            // 
-            this.labelTitulo.AutoSize = true;
-            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.labelTitulo.Location = new System.Drawing.Point(11, 25);
-            this.labelTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelTitulo.Name = "labelTitulo";
-            this.labelTitulo.Size = new System.Drawing.Size(266, 29);
-            this.labelTitulo.TabIndex = 17;
-            this.labelTitulo.Text = "Listado de Contraseñas";
-            // 
-            // botonVer
-            // 
-            this.botonVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.botonVer.Location = new System.Drawing.Point(379, 942);
-            this.botonVer.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
-            this.botonVer.Name = "botonVer";
-            this.botonVer.Size = new System.Drawing.Size(240, 58);
-            this.botonVer.TabIndex = 18;
-            this.botonVer.Text = "Ver";
-            this.botonVer.UseVisualStyleBackColor = true;
             // 
             // ListaClaves
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.botonVer);
-            this.Controls.Add(this.labelTitulo);
-            this.Controls.Add(this.botonCompartir);
             this.Controls.Add(this.botonModificar);
             this.Controls.Add(this.botonEliminar);
+            this.Controls.Add(this.botonCompartir);
+            this.Controls.Add(this.botonVer);
             this.Controls.Add(this.botonAgregar);
+            this.Controls.Add(this.labelTitulo);
             this.Controls.Add(this.tablaClaves);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ListaClaves";
-            this.Size = new System.Drawing.Size(1791, 1048);
+            this.Size = new System.Drawing.Size(1428, 833);
             ((System.ComponentModel.ISupportInitialize)(this.tablaClaves)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -193,11 +211,11 @@ namespace Interfaz
         private System.Windows.Forms.DataGridViewTextBoxColumn Sitio;
         private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
         private System.Windows.Forms.DataGridViewTextBoxColumn UltimaModificacion;
+        private System.Windows.Forms.Label labelTitulo;
         private System.Windows.Forms.Button botonAgregar;
+        private System.Windows.Forms.Button botonVer;
+        private System.Windows.Forms.Button botonCompartir;
         private System.Windows.Forms.Button botonEliminar;
         private System.Windows.Forms.Button botonModificar;
-        private System.Windows.Forms.Button botonCompartir;
-        private System.Windows.Forms.Label labelTitulo;
-        private System.Windows.Forms.Button botonVer;
     }
 }
