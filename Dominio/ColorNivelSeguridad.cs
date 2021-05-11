9032237a12db743e96512b962d719fd03000ce05
Agustina <agustinadisiotvevo@gmail.com>
2021-05-11 17:18:32 -0300

    Agregada clase de ColorNivelSeguridad.

---
+++ Dominio/ColorNivelSeguridad.cs
@@ -0,0 +1,11 @@
+﻿namespace Dominio
+{
+    class ColorNivelSeguridad
+    {
+        public string Rojo { get { return "rojo";} }
+        public string Naranja { get { return "naranja";} }
+        public string Amarillo { get { return "amarillo";} }
+        public string VerdeClaro { get { return "verdeClaro";} }
+        public string VerdeOscuro { get { return "verdeOscuro";} }
+    }
+}
