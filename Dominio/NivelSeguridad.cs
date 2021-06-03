e70c2f92ec4b58b9674313cca6c96e7122b75a0a
Santiago Diaz <SD240926@fi365.ort.edu.uy>
2021-06-03 16:36:42 -0300

    Creada la clase NivelSeguridad y se le agrego la funcion GetNivelSeguridad la cual estaba en Clave.

---
+++ Dominio/NivelSeguridad.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class NivelSeguridad
+    {
+        public string GetNivelSeguridad(String clave)
+        {
+            const int largoRojoMaximo = 7;
+            const int largoNaranjaMaximo = 13;
+            ColorNivelSeguridad color = new ColorNivelSeguridad();
+
+            if (clave.Length <= largoRojoMaximo) return color.Rojo;
+            if (clave.Length <= largoNaranjaMaximo) return color.Naranja;
+
+            bool tieneMin = false,
+                tieneMay = false,
+                tieneNum = false,
+                tieneSim = false;
+
+            foreach (char caracter in clave)
+            {
+                if (!tieneMay || !tieneMin || !tieneMin || !tieneSim)
+                {
+                    tieneMin = tieneMin || VerificadoraString.EsMinuscula(caracter);
+                    tieneMay = tieneMay || VerificadoraString.EsMayuscula(caracter);
+                    tieneNum = tieneNum || VerificadoraString.EsNumero(caracter);
+                    tieneSim = tieneSim || VerificadoraString.EsSimbolo(caracter);
+                }
+                else return color.VerdeOscuro;
+            }
+            if (tieneMin && tieneMay) return (tieneNum && tieneSim) ? color.VerdeOscuro : color.VerdeClaro;
+
+            return color.Amarillo;
+        }
+    }
+}
