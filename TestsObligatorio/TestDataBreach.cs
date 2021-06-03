59da3831078d590f4a6e5b4f72bb078bf4ffedee
Joaquin Meerhoff <jm247096@fi365.ort.edu.uy>
2021-06-03 16:47:53 -0300

    Agregado TestDataBreach para mover todo lo relacionado a databreaches.

---
+++ TestsObligatorio/TestDataBreach.cs
@@ -0,0 +1,24 @@
+﻿using Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsObligatorio
+{
+    [TestClass]
+    class TestDataBreach
+    {
+        [TestInitialize]
+        public void testInit() {
+
+        }
+
+        [TestCleanup]
+        public void testClean() {
+            
+        }
+    }
+}
