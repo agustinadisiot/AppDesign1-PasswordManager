723157855add8a7fd55cc034345caffc17d513e6
Agustina <agustinadisiotvevo@gmail.com>
2021-06-03 18:35:48 -0300

    Agregada funcion para leer archivo y nueva excepcion ArchivoNoExistenteException.

---
+++ Dominio/Exceptions/ArchivoNoExistenteException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Dominio
+{
+    public class ArchivoNoExistenteException : Exception
+    {
+
+        private string message;
+
+        public override string Message
+        {
+            get { return message; }
+        }
+
+        public ArchivoNoExistenteException()
+        {
+            this.message = "No se pudo leer el archivo seleccionado o no existe el archivo.";
+        }
+    }
+}
--- Dominio/LogicaDataBreach.cs
+++ Dominio/LogicaDataBreach.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dominio
 {
-    class LogicaDataBreach
+    public class LogicaDataBreach
     {
+        public List<string> LeerArchivo(string direccion)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(direccion);
+                string linea = sr.ReadLine();
+                string[] clavesYTarjetas = linea.Split('\t');
+                List<string> dataBreach = new List<string>();
+                int cantidadClavesYTarjetas = clavesYTarjetas.Length;
+
+                for (int i = 0; i < cantidadClavesYTarjetas; i++)
+                {
+                    dataBreach.Add(clavesYTarjetas[i]);
+                }
+                sr.Close();
+                return dataBreach;
+            }
+            catch (Exception)
+            {
+                throw new ArchivoNoExistenteException();
+            }
+        }
     }
 }
