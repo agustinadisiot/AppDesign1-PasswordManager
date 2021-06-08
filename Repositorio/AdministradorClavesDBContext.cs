5cce86c30b38a10cb9a7a8fa3b1c7b11504106c2
Agustina <agustinadisiotvevo@gmail.com>
2021-06-08 18:27:09 -0300

    Agregado DataAccessClave.

--- Repositorio/AdministradorClavesDBContext.cs
+++ Repositorio/AdministradorClavesDBContext.cs
@@ -11,6 +11,7 @@ namespace Repositorio
     public class AdministradorClavesDBContext : DbContext
     {
         public DbSet<Tarjeta> Tarjetas { get; set; }
+        public DbSet<Clave> Claves { get; set; }
         public AdministradorClavesDBContext() : base("name=ContextoAdministradorClaves") { }
         
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
---
+++ Repositorio/DataAccessClave.cs
@@ -0,0 +1,65 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class DataAccessClave : IDataAccess<Clave>
+    {
+
+        public void Agregar(Clave entity)
+        {
+            using (var contexto = new AdministradorClavesDBContext())
+            {
+                contexto.Claves.Add(entity);
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Borrar(Clave entity)
+        {
+            using (var contexto = new AdministradorClavesDBContext())
+            {
+                Clave aEliminar = contexto.Claves.FirstOrDefault(t => t.Id == entity.Id);
+                contexto.Claves.Remove(aEliminar);
+                contexto.SaveChanges();
+            }
+        }
+
+        public Clave Get(int id)
+        {
+            using (var contexto = new AdministradorClavesDBContext())
+            {
+                return contexto.Claves.FirstOrDefault(t => t.Id == id);
+            }
+        }
+
+        public IEnumerable<Clave> GetTodos()
+        {
+            using (var contexto = new AdministradorClavesDBContext())
+            {
+                return contexto.Claves.ToList();
+            }
+        }
+
+        public void Modificar(Clave entity)
+        {
+            using (var contexto = new AdministradorClavesDBContext())
+            {
+                Clave aModificar = contexto.Claves.Find(entity.Id);
+                aModificar.UsuarioClave = entity.UsuarioClave;
+                aModificar.Sitio = entity.Sitio;
+                aModificar.Nota = entity.Nota;
+                if (aModificar.Codigo != entity.Codigo)
+                {
+                    aModificar.Codigo = entity.Codigo;
+                    aModificar.FechaModificacion = DateTime.Now.Date;
+                }
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
