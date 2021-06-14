b481f8046a5772084eefc68de5102157f8bdb92f
Agustina <agustinadisiotvevo@gmail.com>
2021-06-14 17:07:08 -0300

    Agregado una interface para los metodos de ingreso del DataBreach.

---
+++ Dominio/IngresoDataBreachUI.cs
@@ -0,0 +1,21 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocio
+{
+    public class IngresoDataBreachUI : IMetodoIngresoDataBreach<string>
+    {
+        public List<Filtrada> DevolverFiltradas(string ingreso)
+        {
+            string[] separadores = new string[] { Environment.NewLine };
+            string[] clavesYTarjetas = ingreso.Split(separadores, StringSplitOptions.None);
+            List<string> dataBreachString = new List<string>(clavesYTarjetas);
+            List<Filtrada> dataBreach = dataBreachString.Select(s => new Filtrada(s)).ToList();
+            return dataBreach;
+        }
+    }
+}
